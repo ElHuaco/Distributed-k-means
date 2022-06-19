367e6cb685481bb8c70c060ea3dc4e782453615f
ElHuaco <alexhuac@gmail.com>
2022-06-19 10:55:54 +0200

    Possible lloyd bug

--- src/dask/notebooks/OUR CODE/distributed_lloyd.py
+++ src/dask/notebooks/OUR CODE/distributed_lloyd.py
@@ -4,6 +4,7 @@ import dask.array as da
 import dask_ml
 
 def lloyd_scalable (X, k, centroids = None, maxIter = 1000, patience = 1e-6):
+    X = make_da(X)
     if centroids is None:
         random_index = np.random.choice(len(X), size=(1, k), replace=False)
         centroids = X[random_index[0]]
@@ -12,10 +13,10 @@ def lloyd_scalable (X, k, centroids = None, maxIter = 1000, patience = 1e-6):
     diff = patience + 1
     loss = 0
     while (epoch < maxIter and loss_diff > patience):
-        distances_matrix = dask_ml.metrics.pairwise_distances(X,
-                np.array(centroids))
+        distances_matrix = dask_ml.metrics.pairwise_distances(X, centroids)
         indeces = da.argmin(distances_matrix, axis=1)
         new_loss = distances_matrix[indeces].sum()
+        # Possible bug if new_centroids = zeros and we don't enter the for loop
         new_centroids = da.zeros((k, n_features))
         for i in range(indeces.max()):
             new_centroids[i] = X[indeces == i].mean(axis=0)
